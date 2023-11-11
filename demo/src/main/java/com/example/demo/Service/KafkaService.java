37febb0b90bc50d62086f54d54f2d421219c67bb
Priyanshu Somani <somanipriyanshu29470@gmail.com>
2023-11-12 00:09:56 +0530

    kafka integrated over Otel

--- demo/src/main/java/com/example/demo/Service/KafkaService.java
+++ demo/src/main/java/com/example/demo/Service/KafkaService.java
@@ -4,16 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+
 @Service
 public class KafkaService {
 
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 
+    @Autowired
+    private Tracer tracer;
+
     public boolean sendMSG(String msg){
-        for(int i=0; i<25; i++){
-            this.kafkaTemplate.send("topic1",msg + i);
+        Span span = tracer.spanBuilder("kafka-producer")
+                .setSpanKind(SpanKind.PRODUCER)
+                .startSpan();
+        try {
+            for(int i=0; i<25; i++){
+                this.kafkaTemplate.send("topic1",msg + i);
+            }
+            return true;
+        } finally {
+            span.end();
         }
-        return true;
+
     }
+
 }
--- demo2/src/main/java/com/example/demo2/Config/KafkaConfig.java
+++ demo2/src/main/java/com/example/demo2/Config/KafkaConfig.java
@@ -2,20 +2,33 @@ package com.example.demo2.Config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
+
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 
 @Configuration
 public class KafkaConfig {
 
-    Logger logger 
-        = LoggerFactory.getLogger(KafkaConfig.class); 
+    Logger logger = LoggerFactory.getLogger(KafkaConfig.class); 
 
-
+    @Autowired
+    private Tracer tracer;
 
     @KafkaListener(topics = "topic1", groupId = "group-1")
     public void getMSG(String msg){
-        logger.info("Log :"+msg); 
+         Span span = tracer.spanBuilder("kafka-consumer")
+                .setSpanKind(SpanKind.CONSUMER)
+                .startSpan();
+        try {
+            logger.info("Log :"+msg); 
+
+        } finally {
+            span.end();
+        }
     }
     
 }
